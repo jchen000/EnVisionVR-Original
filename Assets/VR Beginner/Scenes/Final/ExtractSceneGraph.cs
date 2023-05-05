c69e695ef7fd4329eab4074e977ebb5b21d90c4b
Junlong Chen <junlong2000@outlook.com>
2023-05-05 22:44:03 +0100

    Updated ExtractSceneGraph to show more details of scene graph

--- Assets/VR Beginner/Scenes/Final/ExtractSceneGraph.cs
+++ Assets/VR Beginner/Scenes/Final/ExtractSceneGraph.cs
@@ -2,82 +2,133 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneGraphExtractor : MonoBehaviour
 {
-    [MenuItem("Tools/Extract Scene Graph")]
-    public static void ExtractSceneGraph()
-    {
-        var rootObjects = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
-
-        // Create a list to store the scene graph data
-        List<SceneGraphNode> sceneGraphNodes = new List<SceneGraphNode>();
-
-        foreach (var rootObject in rootObjects)
-        {
-            SceneGraphNode rootNode = ExtractSceneGraphRecursive(rootObject);
-            sceneGraphNodes.Add(rootNode);
-        }
-
-        // Convert the scene graph data to a JSON string
-        string json = JsonUtility.ToJson(new SceneGraphData { sceneGraphNodes = sceneGraphNodes }, true);
-
-        // Save the JSON string to a file
-        string path = Application.dataPath + "/scene_graph.json";
-        File.WriteAllText(path, json);
-
-        Debug.Log("Scene graph saved to " + path);
-    }
-
-    [MenuItem("Tools/Print Scene Graph")]
+    [MenuItem("Scene Graph/Print Scene Graph")]
     public static void PrintSceneGraph()
     {
-        var rootObjects = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
         Debug.Log("Scene Graph:");
-        foreach (var rootObject in rootObjects)
+        PrintGameObjectHierarchy(SceneManager.GetActiveScene().GetRootGameObjects(), 0);
+    }
+
+    [MenuItem("Scene Graph/Export Scene Graph to JSON")]
+    public static void ExportSceneGraphToJson()
+    {
+        // Get the active scene
+        Scene scene = SceneManager.GetActiveScene();
+
+        // Extract the hierarchy of game objects
+        List<GameObjectInfo> gameObjects = new List<GameObjectInfo>();
+        foreach (GameObject rootGameObject in scene.GetRootGameObjects())
         {
-            Debug.Log(rootObject.name);
-            PrintSceneGraphRecursive(rootObject, 1);
+            gameObjects.Add(ExtractGameObjectInfo(rootGameObject));
+        }
+
+        // Serialize the scene graph data as JSON using JsonUtility
+        string json = JsonUtility.ToJson(new SceneGraph(scene.name, gameObjects), true);
+
+        // Write the JSON data to a file
+        string outputPath = Path.Combine(Application.dataPath, "scene-graph.json");
+        File.WriteAllText(outputPath, json);
+
+        Debug.Log("Scene graph saved to " + outputPath);
+    }
+
+    private static void PrintGameObjectHierarchy(GameObject[] gameObjects, int depth)
+    {
+        foreach (GameObject gameObject in gameObjects)
+        {
+            Debug.Log(new string('\t', depth) + gameObject.name);
+
+            // Recursively print the child game objects
+            PrintGameObjectHierarchy(GetChildGameObjects(gameObject), depth + 1);
         }
     }
 
-    private static SceneGraphNode ExtractSceneGraphRecursive(GameObject gameObject)
+    private static GameObject[] GetChildGameObjects(GameObject gameObject)
     {
-        SceneGraphNode node = new SceneGraphNode
+        Transform[] childTransforms = gameObject.GetComponentsInChildren<Transform>();
+        GameObject[] childGameObjects = new GameObject[childTransforms.Length - 1];
+        for (int i = 1; i < childTransforms.Length; i++)
         {
-            name = gameObject.name,
-            children = new List<SceneGraphNode>()
-        };
-
-        foreach (Transform childTransform in gameObject.transform)
-        {
-            SceneGraphNode childNode = ExtractSceneGraphRecursive(childTransform.gameObject);
-            node.children.Add(childNode);
+            childGameObjects[i - 1] = childTransforms[i].gameObject;
         }
-
-        return node;
+        return childGameObjects;
     }
 
-    private static void PrintSceneGraphRecursive(GameObject gameObject, int depth)
+    private static GameObjectInfo ExtractGameObjectInfo(GameObject gameObject)
     {
-        foreach (Transform childTransform in gameObject.transform)
+        List<ComponentInfo> componentInfos = new List<ComponentInfo>();
+        foreach (Component component in gameObject.GetComponents<Component>())
         {
-            string indent = new string(' ', depth * 2);
-            Debug.Log($"{indent}- {childTransform.name}");
-            PrintSceneGraphRecursive(childTransform.gameObject, depth + 1);
+            componentInfos.Add(new ComponentInfo(component.GetType().ToString(), GetSerializedData(component)));
         }
+
+        List<GameObjectInfo> childGameObjectInfos = new List<GameObjectInfo>();
+        foreach (GameObject childGameObject in GetChildGameObjects(gameObject))
+        {
+            childGameObjectInfos.Add(ExtractGameObjectInfo(childGameObject));
+        }
+
+        return new GameObjectInfo(gameObject.name, componentInfos.ToArray(), childGameObjectInfos.ToArray());
+    }
+
+    private static string GetSerializedData(Component component)
+    {
+        SerializedObject serializedObject = new SerializedObject(component);
+        SerializedProperty serializedProperty = serializedObject.GetIterator();
+        serializedProperty.Next(true);
+        while (serializedProperty.Next(false))
+        {
+            if (serializedProperty.propertyType == SerializedPropertyType.ObjectReference)
+            {
+                serializedProperty.objectReferenceValue = null;
+            }
+        }
+        return serializedObject.ToString();
+    }
+
+    [System.Serializable]
+    private class SceneGraph
+    {
+        public string sceneName;
+        public GameObjectInfo[] gameObjects;
+
+        public SceneGraph(string sceneName, List<GameObjectInfo> gameObjects)
+        {
+            this.sceneName = sceneName;
+            this.gameObjects = gameObjects.ToArray();
     }
 }
 
 [System.Serializable]
-public class SceneGraphData
-{
-    public List<SceneGraphNode> sceneGraphNodes;
-}
-
-[System.Serializable]
-public class SceneGraphNode
+private class GameObjectInfo
 {
     public string name;
-    public List<SceneGraphNode> children;
+    public ComponentInfo[] components;
+    public GameObjectInfo[] children;
+
+    public GameObjectInfo(string name, ComponentInfo[] components, GameObjectInfo[] children)
+    {
+        this.name = name;
+        this.components = components;
+        this.children = children;
+    }
 }
+
+[System.Serializable]
+private class ComponentInfo
+{
+    public string type;
+    public string serializedData;
+
+    public ComponentInfo(string type, string serializedData)
+    {
+        this.type = type;
+        this.serializedData = serializedData;
+    }
+}
+}
+
